1ae902d4bdcf8758550ad36add8e8c6d7add2741
João Bispo <vitor.vi10@hotmail.com>
2020-08-03 17:07:46 -0300

    style: Header TeacherList 💄

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg'
 import backIcon from '../../assets/images/icons/back.svg'
 
+import './styles.css';
+
 function TeacherList() {
     return (
-        <div id="page-teacher_list" className="container">
+        <div id="page-teacher-list" className="container">
             <header className="page-header">
                 <div className="top-bar-container">
                     <Link to="/">
