50edeb303c01fd016f2b14623fbc8be35e1483b8
João Bispo <vitor.vi10@hotmail.com>
2020-08-04 20:13:58 -0300

    feat: subjects filter :rocket:

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -11,6 +11,27 @@ interface ScheduleItem {
 
 export default class ClassesController {
 
+async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+    if (!filters.week_day || !filters.subject || !filters.time) {
+        return response.status(400).json({
+            error: 'Missisng filters to search classes'
+        })
+    }   
+
+    const timeInMinutes = convertHourToMinutes(filters.time as string);
+
+    const classes = await db('classes')
+        .where('classes.subject', "=", subject);
+    return response.json(classes);
+    
+}
+
  async create(request: Request, response: Response) {
         const {
             name,
