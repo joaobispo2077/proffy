1f4b92e5d8f499936a36506709ce3aeeed9a75de
João Bispo <vitor.vi10@hotmail.com>
2020-08-04 13:14:52 -0300

    docs: use case routes :memo:

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,21 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        { name: "João", age: "20"},
-        { name: "Vivi", age: "20"}
-    ]
-    return response.json(users);
-    
+app.use(express.json());
+
+app.get('/', (request, response) => {
+
+    return response.json({message: "Hello Word"});    
 });
+
+// app.get('/users', (request, response) => {
+//     const users = [
+//         { name: "João", age: "20"},
+//         { name: "Vivi", age: "20"}
+//     ]
+//     return response.json(users);
+    
+// });
 
 app.listen(3333);
 
