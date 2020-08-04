d68137835ddf34fd0a7ff87be3832da85f688b60
João Bispo <vitor.vi10@hotmail.com>
2020-08-04 11:56:55 -0300

    feat: configured environment :rocket:

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    const users = [
+        { name: "João", age: "20"},
+        { name: "Vivi", age: "20"}
+    ]
+    return response.json(users);
+    
+});
+
+app.listen(3333);
+
+
