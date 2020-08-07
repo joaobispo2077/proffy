68d9a8e1a0080fd07c90b3fb3595236091929f75
João Bispo <vitor.vi10@hotmail.com>
2020-08-07 12:27:38 -0300

    feat: create connections :rocket:

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {    
         id: number;
@@ -15,6 +16,13 @@ export interface TeacherItemProps {
     teacher: Teacher;
 }
 const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
+
     return (  
         <article className="teacher-item">
         <header>
@@ -31,7 +39,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
                 <strong>R$ {teacher.cost}</strong>
             </p>
             {/* adicionar mensagem personalizada: ?text=Eu%20tenho%20interesse%20nas%20aulas%20de%20${teacher.subject}!%20 */}
-            <a href={`https://wa.me/${teacher.whatsapp}`}>
+            <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                 <img src={whatsappIcon} alt="whatsapp"/>
                 Entrar em contato
             </a>
