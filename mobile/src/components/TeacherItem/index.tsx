72b9db3dd55dedf3aedfbcba16f99e4814b3b2fc
João Bispo <vitor.vi10@hotmail.com>
2020-08-08 20:16:58 -0300

    style: TeacherItem :lipstick:

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import { RectButton } from 'react-native-gesture-handler';
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+const TeacherItem = () => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image 
+                    style={styles.avatar}
+                    source={{uri: 'http://github.com/joaobispo2077.png'}}
+                />
+
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>João Bispo</Text>
+                    <Text style={styles.subject}>Matemática</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}> 2 + 2 = 4. {'\n'} {'\n'}
+                Inspirado pelos números visa mostrar que a matemática é muito mais que símbolos complexos, busca conectar a sua mente com inúmeras situações em que os números e calcúlos se apresentam em seu cotidiano e que além de estarem lá, nem são tão complexos assim.
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Custo/hora {'   '}
+                    <Text style={styles.priceValue}>R$ 100,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutlineIcon} />
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entre em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default TeacherItem;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles'
-import PageHeader from '../../components/PageHeader';
 
 function TeacherList() {
         return (
                 <View style={styles.container}>
                         <PageHeader title="Proffys disponíveis" />
+                        <TeacherItem />
                 </View>
         )
 }
