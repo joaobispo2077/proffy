7f3ea9f01a92afd604f767c1a07fbdf345d61753
João Bispo <vitor.vi10@hotmail.com>
2020-08-08 08:08:47 -0300

    feat: tab navigation :rocket:
    to TeacherList & Favorites

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -21,6 +21,10 @@ function Landing(){
         navigate('GiveClasses');        
     }
 
+    function handleNavigateToStudy() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -31,7 +35,10 @@ function Landing(){
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    onPress={handleNavigateToStudy} 
+                    style={[styles.button, styles.buttonPrimary]}
+                >
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
                 </RectButton>
