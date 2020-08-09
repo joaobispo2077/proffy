37a0086cd5242d11f38844778e510e41f512a1e4
João Bispo <vitor.vi10@hotmail.com>
2020-08-09 11:11:07 -0300

    feat: connect api & index total connections :rocket:

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler'
@@ -10,11 +10,21 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 
 
 function Landing(){
     const { navigate } = useNavigation();
+
+    const [totalConnections, setTotalConnecions] = useState(0);
+
+    useEffect(() => {
+        api.get("connections").then( response => {
+           const {total} = response.data;
+           setTotalConnecions(total);            
+        })
+    }, [])
 
 
     function handleNavigateToGiveClassesPage() {
@@ -51,7 +61,7 @@ function Landing(){
                 </RectButton>
             </View>
                 <Text style={styles.totalConnections}>
-                    Total de 214 conexões {' '}
+                    Total de {totalConnections} conexões {' '}
                     <Image source={heartIcon} />
                 </Text>
         </View>    
