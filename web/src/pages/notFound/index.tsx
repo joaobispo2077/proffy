3939e6c9205d969e444612043acce0f3c0363d97
João Bispo <vitor.vi10@hotmail.com>
2020-08-13 18:32:29 -0300

    feat: page 404 :rocket:

--- web/src/pages/notFound/index.tsx
+++ web/src/pages/notFound/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+
+import image404 from "../../assets/images/404.svg";
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function notFound() {
     return (
-        <h1>notFound</h1>
+        <div id="page-404-notFound" className="container">
+            <PageHeader 
+                title="Essa página não foi encontrada, tente retornar para a Home."
+                description="Parece que algum programador tomou café demais :P"
+            >
+            </PageHeader>
+            <div className="image-container">
+                <img src={image404} alt="Página não encontrada"/>
+            </div>
+
+        </div>
     )
 }
 
